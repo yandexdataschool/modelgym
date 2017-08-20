0babbd6d640dc38c43db4eb478df85da705d46ff
andreiSaw <isaevnextdoor@gmail.com>
2017-08-20 19:40:02 +0300

    pytest4

--- tests/test_XGBModel.py
+++ tests/test_XGBModel.py
@@ -6,36 +6,33 @@ import modelgym
 import pickle
 from sklearn.model_selection import train_test_split
 import sklearn.datasets.data
+from modelgym.util import split_and_preprocess
 
 TEST_SIZE = 0.5
 N_CV_SPLITS = 2
 N_ROWS = 1000
-def_params = {}
 TEST_PARAMS = ["classification", "range", "regression"]
 
 
 def test_preprocess_params():
-    global def_params
-    test_params = ["classification", "regression"]
-    for par1 in test_params:
-        model = modelgym.XGBModel(learning_task=par1)
-        if def_params == {}:
-            def_params = model.default_params
+    for par1 in TEST_PARAMS:
+        try:
+            model = modelgym.XGBModel(learning_task=par1)
+        except ValueError:
+            print(par1, "not expected")
         k = len(model.default_params)
-        if model.learning_task == "classification":
-            model.default_params.update({'objective': 'binary:logistic', 'eval_metric': 'logloss', 'silent': 1})
-        elif model.learning_task == "regression":
-            model.default_params.update({'objective': 'reg:linear', 'eval_metric': 'rmse', 'silent': 1})
-        model.default_params['max_depth'] = int(model.default_params['max_depth'])
-        assert test_params.__contains__(model.learning_task)
+        model.preprocess_params(model.default_params)
+        assert TEST_PARAMS.__contains__(model.learning_task)
         assert len(model.default_params) != k + 3
     return 0
 
 
 def test_convert_to_dataset(preprocess):
     X_train, X_test, y_train, y_test = preprocess
-    # iris_data=preprocess
-    global TEST_PARAMS
+    # print ('xtrain',X_train)
+    # print('xtest',X_test)
+    # print('ytrain',y_train)
+
     dtrain = xgboost.DMatrix(X_train, y_train)
     dtest = xgboost.DMatrix(X_test, y_test)
     model = modelgym.XGBModel(learning_task=TEST_PARAMS[0])
@@ -47,13 +44,18 @@ def test_convert_to_dataset(preprocess):
 
 def test_fit(preprocess):
     X_train, X_test, y_train, y_test = preprocess
-    dtrain = xgboost.DMatrix(X_train, y_train)
-    dtest = xgboost.DMatrix(X_test, y_test)
-    evals_result = {}
-    params = def_params
-    result = xgboost.train(dtrain=dtrain, params=params, evals=[(dtest, 'test')], evals_result=evals_result,
-                           num_boost_round=params['n_estimators'], verbose_eval=False)
-    assert result.score.call_count == 1
+    evals_result = []
+    cv_pairs, (dtrain, dtest) = split_and_preprocess(X_train.copy(), y_train,
+                                                     X_test.copy(), y_test,
+                                                     cat_cols=[], n_splits=N_CV_SPLITS)
+    model = modelgym.XGBModel(learning_task="classification")
+    params = model.preprocess_params(model.default_params)
+    for dtrain, dtest in cv_pairs:
+        _dtrain = model.convert_to_dataset(dtrain.X, dtrain.y, dtrain.cat_cols)
+        _dtest = model.convert_to_dataset(dtest.X, dtest.y, dtest.cat_cols)
+        _, evals_result = model.fit(params, _dtrain, _dtest, params['n_estimators'])
+        evals_results.append(evals_result)
+    assert len(evals_result) != 0
 
 
 def test_predict():
@@ -71,19 +73,3 @@ def preprocess(read_data):
     iris_data = read_data
     X_train, X_test, y_train, y_test = train_test_split(iris_data.data, iris_data.target, test_size=TEST_SIZE)
     return X_train, X_test, y_train, y_test
-
-
-def test_XGBModel():
-    global TEST_PARAMS
-    for par1 in TEST_PARAMS:
-        try:
-            model = modelgym.XGBModel(learning_task=par1)
-        except ValueError:
-            print(par1, "not expected")
-        param = def_params
-        if model.learning_task == "classification":
-            param.update({'objective': 'binary:logistic', 'eval_metric': 'logloss', 'silent': 1})
-        elif model.learning_task == "regression":
-            param.update({'objective': 'reg:linear', 'eval_metric': 'rmse', 'silent': 1})
-        param['max_depth'] = int(param['max_depth'])
-        assert model.default_params == param
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,4 +1,16 @@
 import pytest
+import sklearn
+import xgboost as xgb
+from sklearn.cross_validation import train_test_split
+
+from modelgym.util import split_and_preprocess
+import modelgym
+
+
+@pytest.fixture
+def read_data():
+    iris = sklearn.datasets.load_iris()
+    return iris  # data and target
 
 
 def test_preprocess_cat_cols():
@@ -9,5 +21,11 @@ def test_elementwise_loss():
     return 0
 
 
-def test_split_and_preprocess():
+def test_split_and_preprocess(read_data):
+    iris_data = read_data
+    X_train, X_test, y_train, y_test = train_test_split(iris_data.data, iris_data.target, test_size=0.5)
+    cv_pairs, (dtrain, dtest) = split_and_preprocess(X_train.copy(), y_train,
+                                                     X_test.copy(), y_test,
+                                                     cat_cols=[], n_splits=2)
+
     return 0
