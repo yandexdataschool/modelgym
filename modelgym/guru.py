2b0e7efe5ca84d3820139ea289b3aada482d7d3d
onaga1958 <alex--zabelin@mail.ru>
2017-11-15 16:35:25 +0300

    refactor guru

--- modelgym/guru.py
+++ modelgym/guru.py
@@ -7,47 +7,141 @@ class Guru:
     """
     This class analyze data trying to find some issues.
     """
-    def __init__(self, print_hints=True):
-        self._print_hints = print_hints
+    _NOT_NUMERIC_KEY = 'not numeric'
+    _NOT_VARIABLE_KEY = 'not variable'
+    _TOO_RARE_KEY = 'too rare'
+    _TOO_COMMON_KEY = 'too common'
+    _SPARSE = 'sparse'
+    _CATEGORIAL = 'categorial'
+    _CLASS_DISBALANCE = 'class disbalance'
 
-    def _get_categorial_and_sparse(self, X, sample_size, category_qoute, sparse_qoute):
-        categorial_candidates = defaultdict(list)
-        sparse_candidates = []
+    _MESSAGE_DICT = {_SPARSE: 'Consider use hashing trick for ' +
+                              'your sparse features, if you haven\'t ' +
+                              'already. Following features are ' +
+                              'supposed to be sparse: ',
+                     _CATEGORIAL: 'Some features are ' +
+                                  'supposed to be categorial. Make sure ' +
+                                  'that all categorial features are in ' +
+                                  'cat_cols',
+                     _NOT_VARIABLE_KEY: 'Following features are not variable: ',
+                     _NOT_NUMERIC_KEY: 'Following features are not numeric: ',
+                     _CLASS_DISBALANCE: 'There is class disbalance. ' +
+                                        'Probably, you can solve it by data ' +
+                                        'augmentation',
+                     _TOO_RARE_KEY: 'Following classes are too rare: ',
+                     _TOO_COMMON_KEY: 'Following classes are too common: '}
+
+    def __init__(self, print_hints=True,
+                 sample_size=None,
+                 category_qoute=0.2,
+                 sparse_qoute=0.8,
+                 class_disbalance_qoute=0.5):
+        """
+        Arguments:
+            sample_size: int
+                number of objects to be used for category
+                and sparsity diagnostic. If None, whole data will be used.
+            category_qoute: 0 < float < 1
+                max number of distinct feature values in sample
+                to assume this feature categorial
+            sparse_qoute: 0 < float < 1
+                zeros portion in sample required to assume this
+                feature sparse
+            class_disbalance_qoute: 0 < float < 1
+                class portion should be distant from the mean
+                to assume this class disbalanced
+        """
+        self._print_hints = print_hints
+        self._sample_size = sample_size
+        self._category_qoute = category_qoute
+        self._sparse_qoute = sparse_qoute
+        self._class_disbalance_qoute = class_disbalance_qoute
+
+    def check_categorial(self, X):
+        """
+        Arguments:
+            X: array-like with shape (n_objects x n_features)
+        Returns:
+            out: dict
+                out['""" + Guru._NOT_NUMERIC_KEY + """']: list
+                    indexes of features which aren't numeric
+                out['""" + Guru._NOT_VARIABLE_KEY + """']: list
+                    indexes of features which are supposed to be not variable
+        """
+        to_find = Guru._CATEGORIAL
+        return self._get_categorial_or_sparse(X, to_find)
+
+    def check_sparse(self, X):
+        """
+        Arguments:
+            X: array-like with shape (n_objects x n_features)
+        Returns:
+            out: list
+                features which are supposed to be sparse
+        """
+
+        to_find = Guru._SPARSE
+        return self._get_categorial_or_sparse(X, to_find)
+
+    def _get_categorial_or_sparse(self, X, to_find):
+        if to_find == Guru._CATEGORIAL:
+            candidates = defaultdict(list)
+        elif to_find == Guru._SPARSE:
+            candidates = []
+        else:
+            raise ValueError('In _get_categorial_or_sparse to_find must be ' +
+                             Guru._CATEGORIAL + ' or ' + Guru._SPARSE)
+
         for i in range(len(X[0])):
             feature = [obj[i] for obj in X]
             if not (isinstance(feature[0], float)
                     or isinstance(feature[0], int)):
-                categorial_candidates['not numeric'].append(i)
+                if to_find == Guru._CATEGORIAL:
+                    candidates[Guru._NOT_NUMERIC_KEY].append(i)
             else:
-                if (sample_size is not None and
-                        sample_size < len(feature)):
+                if (self._sample_size is not None and
+                        self._sample_size < len(feature)):
                     sample = np.random.choice(feature,
-                                              sample_size,
+                                              self._sample_size,
                                               False)
                 else:
                     sample = feature
                 counter = Counter(sample)
-                # remove zeros from sample in order to avoid detecting sparse
-                # features as categorial
-                cat_quote = (len(sample) - counter[0]) * category_qoute
-                if len(counter) > 1 and len(counter) - 1 < cat_quote:
-                    categorial_candidates['not variable'].append(i)
-                elif counter[0] > len(sample) * sparse_qoute:
-                    sparse_candidates.append(i)
 
-        return categorial_candidates, sparse_candidates
+                if to_find == Guru._CATEGORIAL:
+                    # remove zeros from sample in order to avoid detecting sparse
+                    # features as categorial
+                    cat_quote = (len(sample) - counter[0]) * self._category_qoute
+                    if len(counter) > 1 and len(counter) - 1 < cat_quote:
+                        candidates[Guru._NOT_VARIABLE_KEY].append(i)
+                elif counter[0] > len(sample) * self._sparse_qoute:
+                    candidates.append(i)
 
-    def _get_disbalanced_classes(self, y, class_balance_qoute):
+        self._print_warning(candidates, Guru._MESSAGE_DICT[to_find])
+        return candidates
+
+    def check_class_disbalance(self, y):
+        """
+        Arguments:
+            y: array-like with shape (n_objects,)
+        Returns:
+            out['""" + Guru._TOO_COMMON_KEY + """']: list
+                too common classes
+            out['""" + Guru._TOO_RARE_KEY + """']: list
+                too rare classes
+        """
         candidates = defaultdict(list)
         counter = Counter(y)
-        upper = len(y) / len(counter) / class_balance_qoute
-        lower = len(y) / len(counter) * class_balance_qoute
+        upper = len(y) / len(counter) / self._class_disbalance_qoute
+        lower = len(y) / len(counter) * self._class_disbalance_qoute
 
         for label, cnt in counter.items():
             if cnt > upper:
-                candidates['too common'].append(label)
+                candidates[Guru._TOO_COMMON_KEY].append(label)
             if cnt < lower:
-                candidates['too rare'].append(label)
+                candidates[Guru._TOO_RARE_KEY].append(label)
+
+        self._print_warning(candidates, Guru._MESSAGE_DICT[Guru._CLASS_DISBALANCE])
 
         return candidates
 
@@ -59,83 +153,39 @@ class Guru:
                     if self._print_hints:
                         print(warning)
                     break
+            for key in elements.keys():
+                self._print_warning(elements[key], Guru._MESSAGE_DICT[key])
         else:
             if len(elements) > 0:
                 self.no_warnings = False
                 if self._print_hints:
                     print(warning, elements)
 
-    def check_data(self, data, sample_size=None,
-                   category_qoute=0.2,
-                   sparse_qoute=0.8,
-                   class_balance_qoute=0.5,
-                   check_categorial_features=True,
-                   check_class_disbalance=True,
-                   check_sparsity=True):
+    def check_everything(self, data):
         """
         Arguments:
             data: XYCDataset-like
-            data.X: array-like with shape (n_objects x n_features)
-            daya.y: array-like with shape n_objects
-            sample_size: int
-                number of objects to be used for category
-                and sparsity diagnostic. If None, whole data will be used.
-            category_qoute: 0 < float < 1
-                max number of distinct feature values in sample
-                to assume this feature categorial
-            sparse_qoute: 0 < float < 1
-                zeros portion in sample required to assume this
-                feature sparse
-            class_balance_qoute: 0 < float < 1
-                class portion should be distant from the mean
-                to assume this class disbalanced
-            check_categorial_features: bool
-                check if data.X has categorial features
-            check_sparsity: bool
-                check if data.X has sparse features
-            check_class_disbalance: bool
-                check if data.y has class disbalance
-
         Returns:
             (categorials, sparse, disbalanced)
                 categorials: indexes of features which are supposed to be categorial
                 sparse: indexes of features which are supposed to be sparse
                 disbalanced: disbalanced classes
+            For more detailes see respective methods:
+                check_categorials
+                check_sparse
+                check_class_disbalance
         """
 
         categorials, sparse, disbalanced = [None] * 3
         self.no_warnings = True
-        if check_categorial_features or check_sparsity:
-            _categorials, _sparse = self._get_categorial_and_sparse(data.X,
-                                                                    sample_size,
-                                                                    category_qoute,
-                                                                    sparse_qoute)
-            if check_sparsity:
-                sparse = _sparse
-                self._print_warning(sparse, 'Consider use hashing trick for ' +
-                                    'your sparse features, if you haven\'t ' +
-                                    'already. Following features are ' +
-                                    'supposed to be sparse: ')
-            if check_categorial_features:
-                categorials = _categorials
-                self._print_warning(categorials, 'Some features are ' +
-                                    'supposed to be categorial. Make sure ' +
-                                    'that all categorial features are in ' +
-                                    'cat_cols')
-                self._print_warning(categorials['not numeric'],
-                                    'Following features are not numeric: ')
-                self._print_warning(categorials['not variable'],
-                                    'Following features are not variable: ')
+        if self._check_sparsity:
+            sparse = self.check_sparse(data.X)
 
-        if check_class_disbalance:
-            disbalanced = self._get_disbalanced_classes(data.y, class_balance_qoute)
-            self._print_warning(disbalanced, 'There is class disbalance. ' +
-                                'Probably, you can solve it by data ' +
-                                'augmentation')
-            self._print_warning(disbalanced['too common'],
-                                'Following classes are too common: ')
-            self._print_warning(disbalanced['too rare'],
-                                'Following classes are too rare: ')
+        if self._check_categorial_features:
+            categorials = self.check_categorials(data.X)
+
+        if self._check_class_disbalance:
+            disbalanced = self.check_class_disbalance(data.y)
 
         if self.no_warnings and self._print_hints:
             print('Everything is allright!')
--- tests/test_guru.py
+++ tests/test_guru.py
@@ -1,61 +1,50 @@
 from modelgym import Guru
 from sklearn.datasets import load_breast_cancer
-from collections import namedtuple
 
 
-XYCDataset = namedtuple('data', ['X', 'y', 'cat_cols'])
-X = [['joke', 1231, 0.12312, 1, True, 0, 0],
-     ['lol', 123, 1231.1231, 1, False, 0, 0],
-     ['not joke', 1313, 12.133, 1, False, 1, 0],
-     ['joke', 12312, 0.24183, 0, True, 0, 0],
-     ['silly joke', 321, 0.12, 1, False, 0, 0],
-     ['silly joke', 32, 0.2, 1, False, 0, 0],
-     ['silly joke', 31, 0.1, 1, True, 0, 1.1],
-     ['silly joke', 21, 0.123, 1, True, 0, 4.6],
-     ['silly joke', 21, 0.123, 1, True, 0, 0],
-     ['silly joke', 1, 0.124, 1, True, 0, -2.73]]
-y = [0]*8 + [1] + [3]*2
-_TOY_DATASET = XYCDataset(X, y, None)
+_TOY_X = [['joke', 1231, 0.12312, 1, True, 0, 0],
+          ['lol', 123, 1231.1231, 1, False, 0, 0],
+          ['not joke', 1313, 12.133, 1, False, 0, 0],
+          ['joke', 12312, 0.24183, 0, True, 1, 0],
+          ['silly joke', 321, 0.12, 1, False, 0, 0],
+          ['silly joke', 32, 0.2, 1, False, 0, 0], ['silly joke', 31, 0.1, 1, True, 0, 1.1],
+          ['silly joke', 21, 0.123, 1, True, 0, 4.6],
+          ['silly joke', 21, 0.123, 1, True, 0, 0],
+          ['joke', 1, 0.23, 1, False, 0, 0],
+          ['silly joke', 1, 0.124, 1, True, 0, -2.73]]
+_TOY_Y = [0]*8 + [1] + [3]*2
+_BREAST_X, _BREAST_Y = load_breast_cancer(True)
 
 
-def _empty(obj):
-    if isinstance(obj, dict):
-        for i in obj.values():
-            if not _empty(i):
-                return False
-    elif isinstance(obj, list) or isinstance(obj, tuple):
-        for i in obj:
-            if not _empty(i):
-                return False
-    else:
-        return True
-    return True
+def test_sparse():
+    guru_params = [{}, {}, {'sparse_qoute': 0.5}]
+    Xs = [_BREAST_X] + [_TOY_X] * 2
+    answers = [[], [5], [5, 6]]
+
+    _test(lambda guru, data: guru.check_sparse(data), guru_params, Xs, answers)
 
 
-def test_without_warnings():
-    guru = Guru(print_hints=False)
-    X, y = load_breast_cancer(True)
-    result = guru.check_data(XYCDataset(X, y, None))
-    assert _empty(result)
+def test_categorial():
+    guru_params = [{}, {}, {'category_qoute': 0.15}]
+    Xs = [_BREAST_X] + [_TOY_X] * 2
+    answers = [{}, {Guru._NOT_NUMERIC_KEY: [0], Guru._NOT_VARIABLE_KEY: [3, 4]},
+               {Guru._NOT_NUMERIC_KEY: [0], Guru._NOT_VARIABLE_KEY: [3]}]
+    _test(lambda guru, data: guru.check_categorial(data), guru_params, Xs, answers)
 
 
-def test_warnings():
-    guru = Guru(print_hints=False)
-    result = guru.check_data(_TOY_DATASET, category_qoute=0.5)
-    assert result == ({'not numeric': [0], 'not variable': [3, 4]},
-                      [5], {'too rare': [1], 'too common': [0]})
+def test_class_disbalance():
+    guru_params = [{}, {}, {'class_disbalance_qoute': 0.4},
+                   {'class_disbalance_qoute': 0.8}]
+    ys = [_BREAST_Y] + [_TOY_Y] * 2
+    answers = [{},
+               {Guru._TOO_COMMON_KEY: [0], Guru._TOO_RARE_KEY: [1]},
+               {Guru._TOO_RARE_KEY: [1]},
+               {Guru._TOO_COMMON_KEY: [0], Guru._TOO_RARE_KEY: [1, 3]}]
+    _test(lambda guru, data: guru.check_class_disbalance(data), guru_params, ys, answers)
 
 
-def test_sparse_arguments():
-    guru = Guru(print_hints=False)
-
-    params = [{'check_sparsity': False},
-              {'sparse_qoute': 0.3}]
-    answers = [(None, None, None),
-               (None, [5, 6], None)]
-
-    for param, answer in zip(params, answers):
-        assert answer == guru.check_data(_TOY_DATASET,
-                                         check_categorial_features=False,
-                                         check_class_disbalance=False,
-                                         **param)
+def _test(function, guru_params, datasets, answers):
+    for params, data, answer in zip(guru_params, datasets, answers):
+        guru = Guru(print_hints=False, **params)
+        result = function(guru, data)
+        assert result == answer
