3a510f6e32ce480d95313a0de6a6e1ebfe21fe9a
onaga1958 <alex--zabelin@mail.ru>
2017-12-14 00:23:41 +0300

    small changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,8 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
 
+autoclass_content = 'both'
+
 
 # -- General configuration ------------------------------------------------
 
@@ -96,20 +98,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-# Custom sidebar templates, must be a dictionary that maps document names
-# to template names.
-#
-# This is required for the alabaster theme
-# refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
-html_sidebars = {
-    '**': [
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-    ]
-}
-
+# html_static_path = ['_static']
 
 # -- Options for HTMLHelp output ------------------------------------------
 
