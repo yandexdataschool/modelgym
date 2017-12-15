4c0d83b71b271b52412f811f3c22ab62e0861e11
Daniil Dmitriev <ocksumoron@yandex.ru>
2017-12-15 22:03:08 +0300

    Refactored Report class. Added test for it. Pt2

--- modelgym/report.py
+++ modelgym/report.py
@@ -4,10 +4,17 @@ import matplotlib.pyplot as pyplot
 import pandas as pd
 
 from modelgym.metrics import Metric
-from modelgym.util import calculate_custom_metrics
 
 
 class Report:
+    
+    def __init__(self, results, test_set, metrics):
+        self._results = results
+        self._test_set = test_set
+        self._metric_results = {metric: self._parse_results(metric.name) 
+                                for metric in metrics}
+        
+    """  
     def __init__(self, results, models_dict, models_holder, test_set, metrics, task_type):
         self._models_dict = models_dict
         self._test_set = test_set
@@ -19,11 +26,15 @@ class Report:
         self._metric_bounds = {metric: self._parse_results(results,
                                                            metric.name + '_bounds') 
                                   for metric in metrics}
+                                  """
    
-    def _parse_results(self, results, field):
-        return np.array([(results[i]['default'][field], results[i]['tuned'][field])
-                         for i in self._models_dict.keys()])
-   
+    def _parse_results(self, field):
+        result = []
+       
+        for model_result in self._results.values():
+            result.append(np.array([score[field] for score in model_result['result']['metric_cv_results']]).mean())     
+        return np.array(result)
+    
     def _calc_differences(self, metric):
         baseline = self._metric_results[metric].max() if metric.is_min_optimal else\
                    self._metric_results[metric].min()
@@ -39,10 +50,9 @@ class Report:
                "~" * (tilda_length if (line_length - len(name) - space_count) % 2 == 0 else tilda_length + 1) + "\n")
         
     def _print_differences_dataframe(self, metric, diffs):
-        formatted = [['{:.6f} ({:+.2f}%)'.format(self._metric_results[metric][i, j], 
-                                                 diffs[i, j]) for j in range(2)] 
-                              for i in range(len(self._models_dict.keys()))]
-        print (pd.DataFrame(formatted, columns=['default', 'tuned'], index=self._models_dict.keys()))
+        formatted = ['{:.6f} ({:+.2f}%)'.format(self._metric_results[metric][i], diffs[i]) 
+                     for i in range(len(self._results.keys()))]
+        print (pd.DataFrame(formatted, columns=['tuned'], index=self._results.keys()))
 
     def _validate_metric(self, metric):
         if metric not in self._metric_results:
@@ -66,29 +76,19 @@ class Report:
         
     def _plot_metric_results(self, metric):
         """internal function to exclude meta information about report task going on"""
-        cartesian = lambda x, y: np.transpose([np.tile(x, len(y)), np.repeat(y, len(x))])
         
         # stack position to sorting them and plotting afterwards
-        number_of_models = len(self._models_dict.keys())
-
-        tiled_models = cartesian(np.arange(2), np.arange(number_of_models))[:, ::-1]
-  
-        sorted_tiled_models = np.array(sorted(tiled_models, 
-                                           key=lambda row: self._metric_results[metric][row[0]][row[1]],
-                                           reverse=not metric.is_min_optimal))
+        number_of_models = len(self._results.keys())
+        
         pyplot.figure(figsize=(20, 7))
-        for i in np.arange(number_of_models):
-            positions = np.where(sorted_tiled_models[:, 0] == i)[0]
-            models_indices = sorted_tiled_models[positions][:, 1]
-            values = self._metric_results[metric][i][models_indices]
-            errors = self._metric_bounds[metric][i][models_indices].reshape(2, -1)
-            pyplot.errorbar(positions, values, yerr=errors, fmt='.', markersize=25, capsize=10, capthick=2, barsabove=True)
-        
-        modes = np.array(['default', 'tuned'])
-        xticks = [list(self._models_dict.keys())[row[0]] + ' ' + modes[row[1]] +\
-                 '\n%.5f' % self._metric_results[metric][row[0]][row[1]]
-                            for row in sorted_tiled_models]
-        
+        xticks = []
+        for i, (model_name, model_result) in enumerate(self._results.items()):
+            values = self._metric_results[metric][i]
+            cv_results = np.array([score[metric.name] for score in model_result['result']['metric_cv_results']])
+            errors = np.fabs(np.array([cv_results.min(), cv_results.max()]).reshape(2, -1) - values)
+            pyplot.errorbar(i, values, yerr=errors, fmt='.', markersize=25, capsize=10, capthick=2, barsabove=True)
+            xticks.append(model_name + ' tuned\n%.5f' % self._metric_results[metric][i])
+            
         pyplot.xticks(range(len(xticks)), xticks, fontsize=15)
         pyplot.yticks(fontsize=12)
         pyplot.title('Comparison', fontsize=20)
@@ -115,13 +115,12 @@ class Report:
         self._validate_metric(metric)
         predictions = []
         names = []
-        for i, model_class in self._models_dict.items():
-            model = model_class(self._task)
-            name = model.get_name()
-            names.append(name)
-            estimator = self._models_holder[name].state['tuned_test']['bst']
-            _dtest = model.convert_to_dataset(self._test_set.X, self._test_set.y, self._test_set.cat_cols)
-            predictions.append(metric.get_predictions(model, estimator, _dtest, self._test_set))
+        for model_name, model_class in self._results.items():
+            model = model_class['model_space'].model_class(params=model_class['result']['params'])
+            names.append(model_name)
+            train_set, for_corr_set = self._test_set.split(2)
+            model.fit(train_set)
+            predictions.append(model.predict_proba(for_corr_set))
 
         seaborn.set_style('ticks')
         fig, ax = pyplot.subplots()
