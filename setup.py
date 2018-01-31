b045f8cf50766977ab25075dc741444d2919d34f
Andrey Ustyuzhanin <andrey.u@gmail.com>
2018-01-31 10:16:58 +0300

    pytest -> requirements-dev

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(name="modelgym",
       packages=["modelgym", "modelgym.utils", "modelgym.trainers", "modelgym.trackers", "modelgym.models"],
       install_requires=["numpy", "scipy", "scikit-learn>=0.18", "matplotlib",
                         "pandas", "seaborn", "networkx==1.11", "hyperopt==0.1",
-                        "pymongo==3.4.0", "pyyaml==3.12", "pytest==3.2.1",
+                        "pymongo==3.4.0", "pyyaml==3.12",
                         "scikit-optimize==0.4", "catboost==0.5"]
       )
